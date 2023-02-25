2a4bb27e58f8b74949b258323ef1db127c895062
Austin Suarez <austinmsuarez@gmail.com>
2023-02-25 16:20:35 -0600

    Initial Commit

---
+++ MyWeather/MyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherApp.swift
+//  MyWeather
+//
+//  Created by Austin Suarez on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
